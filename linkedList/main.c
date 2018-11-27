354528f159a054ea39d90212745161b104894f7c
cmj <vcmj@foxmail.com>
2018-11-27 14:16:02 +0800

    增加链表的简单搜索代码。

--- linkedList/main.c
+++ linkedList/main.c
@@ -1,4 +1,4 @@
-//编译器名: TDM-GCC 4.8.1 32-bit Debug
+//编译器名: TDM-GCC 4.8.1 32-bit Debug 
 #include "node.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,6 +16,7 @@ typedef struct _list{
 } List;//将head指针变量放入List结构体，方便将来对链表的结构扩充，比如增加结构tail指针。 
 
 void add(List* pList,int number);
+void print(List* pList);
 
 int main(int argc, char *argv[]) {
 	List list;
@@ -25,10 +26,35 @@ int main(int argc, char *argv[]) {
 	do{
 		scanf("%d",&number);
 		if(number != -1){
-			add(&list,number);//传入List地址比传入head指针变量的地址好。 
+			add(&list,number);
 		}						     
 	} while(number != -1);
 	
+//	Node *p;
+//	for(p=list.head;p;p=p->next){//链表操作中常见的经典写法，只要p还存在就不断循环遍历。 
+//		printf("%d\t",p->value);
+//	}
+//	printf("\n");
+	print(&list);
+	
+	//链表的简单搜索 
+	scanf("%d",&number);
+	 
+	Node *p;
+	int isFound = 0;
+	for(p=list.head;p;p=p->next){
+		if(p->value==number){
+			printf("找到了!\n");
+			isFound = 1;
+			break;
+		}
+	}
+	
+	if(!isFound){
+		printf("没找到！\n");
+	}
+	
+		
 	return 0;
 }
 
@@ -51,3 +77,15 @@ void add(List* pList,int number)//传入的是结构的地址。
 			pList->head = p;
 		}
 }
+
+void print(List* pList){
+	Node *p;
+	for(p=pList->head;p;p=p->next){//链表操作中常见的经典写法，只要p还存在就不断循环遍历。 
+		printf("%d\t",p->value);
+	}
+	printf("\n");
+}
+
+
+
+
