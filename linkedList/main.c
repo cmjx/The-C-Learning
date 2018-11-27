d7c1cfb6d91399416b29c3ff62df43e7b698b8ad
cmj <vcmj@foxmail.com>
2018-11-27 13:54:03 +0800

    改进链表函数，将head指针变量放入List结构，为将来改进扩展提供可能。工程化程序思维。

--- linkedList/main.c
+++ linkedList/main.c
@@ -8,29 +8,38 @@
 //	struct _node *next;
 //} Node;
 
-Node* add(Node** pHead,int number);
+typedef struct _list{
+	Node* head;
+// Node* tail; //通过定义自己的数据结构代表整个链表 
+// 给我们将来升级， 将来改进List带来了无限可能。 
+//如果没有这个结构，我们只是一个悬在外面的 head，这是工程化程序思维。  
+} List;//将head指针变量放入List结构体，方便将来对链表的结构扩充，比如增加结构tail指针。 
+
+void add(List* pList,int number);
 
 int main(int argc, char *argv[]) {
-	Node* head = NULL;
+	List list;
+	list.head = NULL;
+//	list.tail = NULL;
 	int number;
 	do{
 		scanf("%d",&number);
 		if(number != -1){
-			head = add(&head,number);//传入head指针变量的地址，这样调用者赋值不赋值都没关系了。 
+			add(&list,number);//传入List地址比传入head指针变量的地址好。 
 		}						     
 	} while(number != -1);
 	
 	return 0;
 }
 
-Node* add(Node** pHead,int number)//传入的是指针变量的地址。 
-{							      //这样对*pHead操作就能操作外部指针变量 
+void add(List* pList,int number)//传入的是结构的地址。 
+{							     
 		// add to linked-list
 		Node *p = (Node*)malloc(sizeof(Node));
 		p->value = number;
 		p->next = NULL;
 		//find the last
-		Node *last = *pHead;
+		Node *last = pList->head;
 		if(last){
 			while(last->next){
 				last = last->next;
@@ -39,7 +48,6 @@ Node* add(Node** pHead,int number)//传入的是指针变量的地址。
 			last->next = p;
 		}
 		else{
-			*pHead = p;
+			pList->head = p;
 		}
-		return *pHead;//是否return已经没关系了。 
 }
